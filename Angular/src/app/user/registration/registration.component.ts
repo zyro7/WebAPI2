7de62af5e8158e2bbd666b51615c15a232d42200
zyro7 <zyro7@hotmail.com>
2019-07-08 10:54:08 +0200

    Register done

---
+++ Angular/src/app/user/registration/registration.component.ts
@@ -0,0 +1,40 @@
+import { UserService } from './../../shared/user.service'
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styles: []
+})
+export class RegistrationComponent implements OnInit {
+
+  constructor(public service: UserService) { }
+
+  ngOnInit() {
+    this.service.formModel.reset();
+  }
+
+  onSubmit(){
+    this.service.register().subscribe(
+      (res:any) =>{
+        if(res.succeded){
+          this.service.formModel.reset();
+        }else{
+          res.errors.forEach(element => {
+            switch (element.code) {
+              case 'DuplicateUserName':
+                
+                break;
+            
+              default:
+                break;
+            }
+          });
+        }
+      },
+      err =>{
+        console.log(err);
+      }
+    )
+  }
+}
---
+++ WebAPI2/WebAPI2/Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebAPI2.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Column(TypeName = "nvarchar(150)")]
+        public string FullName { get; set; }
+    }
+
+
+}
